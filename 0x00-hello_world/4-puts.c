cdffdee1121d5b4f6b0ae47e9b13acc96dfac62d
shuayb1 <shuaybpopoola@gmail.com>
2022-10-28 14:27:10 -0700

    C program that prints, Use the function puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,6 +7,6 @@ int main(void)
 	 *Return 0 ends the program
 	 * */
 
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
