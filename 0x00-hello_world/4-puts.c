21c49729c053de07c1a403b96d9ad1d9c7f387c6
shuayb1 <shuaybpopoola@gmail.com>
2022-10-28 14:24:00 -0700

    C program that prints, Use the function puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	/*
+	 *main prints themessage
+	 *Return 0 ends the program
+	 * */
+
+	puts("\"Programming is like building a multilingual puzzle\n");
+	retrun (0);
+}
